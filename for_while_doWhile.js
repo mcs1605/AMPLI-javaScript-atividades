c8fd30bf7295c2caf73634fc47aa8c740dea4e16
Marcos Paulo <50648583+mcs1605@users.noreply.github.com>
2025-06-02 23:25:25 -0300

    for while e do while

--- for_while_doWhile.js
+++ for_while_doWhile.js
@@ -1,3 +1,22 @@
-for(i = 0, i < 6, i = i + 1){
+//LAÇO DE REPETIÇÃO
+for(i = 0; i < 6; i = i + 1){
     console.log(i);
 }
+console.log("\n");
+
+//PÁRA ANTES DO NÚMERO DA CONDIÇÃO
+for(i = 0; i < 6; i = i + 1){
+    if(i == 2){
+        break;
+    }
+    console.log(i);
+}
+console.log("\n");
+
+//PULA O NÚMERO DA CONDIÇÃO
+for(i = 0; i < 6; i = i + 1){
+    if(i == 2){
+        continue;
+    }
+    console.log(i);
+} 
